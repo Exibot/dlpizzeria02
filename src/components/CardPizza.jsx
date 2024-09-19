f52aa7931623622de827bbccb7398d211e27c727
Enrique Altamirano <enrique.altamiros@gmail.com>
2024-09-19 14:36:51 -0300

    actualizando repo

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,23 +1,33 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
-
-
+import Button from "react-bootstrap/Button";
 
 const CardPizza = ({ name, price, ingredients, img }) => {
     const ingredientsString = ingredients.join(", ");
 
-  // TODO: Add functionality to handle price formatting and add a button to add the pizza to the cart.
-  return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={img} />
-      <Card.Body>
-        <Card.Title>Pizza {name}</Card.Title>
-        <ListGroup variant="list-group-flush">
-          <ListGroup.Item>Ingredientes: <p>{ingredientsString}</p></ListGroup.Item>
-          <ListGroup.Item>Precio: ${price}</ListGroup.Item>
-        </ListGroup>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Title>Pizza {name}</Card.Title>
+            </Card.Body>
+            <ListGroup variant="list-group-flush">
+                <ListGroup.Item>
+                    <h5>Ingredientes:</h5> <p>{ingredientsString}</p>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <h4>Precio: ${price}</h4>
+                    <div className="d-flex justify-content-between">
+                        <Button variant="outline-dark" size="sm">
+                            Ver Más
+                        </Button>
+                        <Button variant="dark" size="sm">
+                            Añadir
+                        </Button>
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
+        </Card>
+    );
 };
 export default CardPizza;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import '../assets/Header.css'
 const Header = () => {
     return (
-        <header className="header">
+        <header className="header mb-5">
             <div className='header-content'>
                 <h1>¡Pizzería Mama Mía</h1>
                 <p>Tenemos las mejores pizzas del mundo!</p>
